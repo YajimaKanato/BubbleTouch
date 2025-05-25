55f1a80ca4ab7d427ecc835b28b36e6a3f987877
KNTy1018 <soramametaro1018@gmail.com>
2025-05-25 18:55:50 +0900

    [add]泡生成のスクリプトを追加

---
+++ Assets/Scripts/BubbleSpawner.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+class BubbleData
+{
+    [Header("BubblePrefab")]
+    [Tooltip("生成する泡のプレハブを設定")]//カーソルを合わせると説明が出てくる
+    public GameObject prefab;
+
+    [Range(0.0f, 1.0f)]//スライダーが出てくる
+    [Tooltip("泡の出現率")]
+    public float weight = 1.0f;
+}
+public class BubbleSpawner : MonoBehaviour
+{
+    [Header("Prefab List with Probability")]
+    [Tooltip("プレハブと確率を登録してください")]
+    [SerializeField]
+    List<BubbleData> list = new List<BubbleData>();
+    //BubbleData型のリストを作成
+    //リストにしたのは種類が増えても対応できるように
+
+    [Header("Spawn Settings")]
+    [Tooltip("生成間隔")]
+    [SerializeField]
+    float spawnInterval = 1.0f;
+    //スポーン間隔を設定
+
+    Coroutine spawnCoroutine;//コルーチン
+
+    private void Start()
+    {
+        spawnCoroutine = StartCoroutine(SpawnPrefabLoop());
+    }
+
+    IEnumerator SpawnPrefabLoop()//これはコルーチンを作るにあたって必須
+    {
+        while (true)
+        {
+            SpawnRandomPrefab();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    void SpawnRandomPrefab()//プレハブをランダムに生成するメソッド
+    {//重み付き確率で実装
+        if (list == null || list.Count == 0)
+        {
+            Debug.LogWarning("泡のリストが空です");
+            //LogWarningは危険信号をコンソールに出せる
+            return;
+        }
+
+        float totalWeight = 0.0f;//重みの総和
+
+        foreach (var bubble in list)
+        {//重みの総和の計算
+            totalWeight += bubble.weight;
+        }
+
+        float rand = Random.Range(0.0f, totalWeight);
+        //生成泡の乱数値
+        float currentWeight = 0.0f;
+        //現在の重みを保持
+
+        foreach (var bubble in list)
+        {
+            currentWeight += bubble.weight;//重みを更新
+            if (rand <= currentWeight)
+            {//重みの幅に乱数値が含まれた時
+                Instantiate(bubble.prefab, transform.position, Quaternion.identity);
+                //加えた重みを持つ泡のプレハブを生成
+                //座標はスポナーの位置
+                //Quaternion.identityで回転なし
+                break;
+            }
+        }
+    }
+
+    void StopSpawning()
+    {
+        if (spawnCoroutine != null)
+        {//コルーチンが何か動いていれば？
+            StopCoroutine(spawnCoroutine);
+            spawnCoroutine = null;
+        }
+    }
+}
